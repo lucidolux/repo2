97b7ac5aea3d8fedf5ac6f87b1356ed505f30b8a
sebastian <sebastian@sebastian-PC>
2019-11-29 21:19:59 -0300

    interfaces // repositorios y parte del test

--- Tp2/src/main/java/sebastian/java/Tp2/test/TestRespositoryTp2.java
+++ Tp2/src/main/java/sebastian/java/Tp2/test/TestRespositoryTp2.java
@@ -18,81 +18,81 @@ import sebastian.java.Tp2.repositories.interfaces.jdbc.ResponsableRepository;
 import sebastian.java.Tp2.repositories.interfaces.jdbc.VeterinarioRepository;
 public class TestRespositoryTp2 {
     public static void main(String[] args) {
-//        try (Connection conn=Connector.getConnection()){
-//            I_GatoRepository gr= new GatoRepository(conn);
+        try (Connection conn=Connector.getConnection()){
+            I_GatoRepository gr= new GatoRepository(conn);
 
-//             //***********************SAVE************************"
-//            Gato gato=new Gato(
-//                      "canguro"
-//                    , "mestizo"
-//                    , 12
-//                    , Genero.hembra
-//                    , 2
-//                    , Adopcion.adoptado
-//                    , 1
-//                    , "2018-06-11");
-//            gr.save(gato);
-//            System.out.println(gato);
-//            
-//             //"**************************REMOVE*********************************");
-//                       gr.remove(gr.getByIdGato(35));
-//
-//           
-//             //"************************UPDATE***********************************");
-// 
-//                       gato = gr.getByIdGato(2);
-//                       gato.setNombre("lucas");
-//                       gato.setRaza("mestizo");
-//                       gato.setEdad(4);
-//                       gato.setGenero(Genero.hembra);
-//                       gato.setIdVeterinario(2);
-//                       gato.setAdopcion(Adopcion.adoptado);
-//                       gato.setIdResponsable(3);
-//                       gato.setF_adopcion("2015-07-04");
-//                       gr.update(gato);
-//                       gr.getAll().forEach(System.out::println); // getALl
-//   
-//           } catch (Exception e) { e.printStackTrace(); }
-//    
-        
-//        //**************************PERROS**************************************
-//        try (Connection conn= Connector.getConnection()){
-//            I_PerroRepository pr=new PerroRepository(conn);
-//            
-//        //********************SAVE****************************
-//             Perro perro = new Perro(
-//                  "kusturika"
-//                 , "labrador"  
-//                 , 11
-//                 , Genero.hembra
-//                 , 4
-//                 , Adopcion.adoptado
-//                 , 3
-//                 ,"2018-10-23");
-//            pr.save(perro);
-//            System.out.println(perro);
-//            
-//            **********************************REMOVE***************************
-//            pr.remove(pr.getByIdPerro(3));
+             //***********************SAVE************************"
+            Gato gato=new Gato(
+                      "canguro"
+                    , "mestizo"
+                    , 12
+                    , Genero.hembra
+                    , 2
+                    , Adopcion.adoptado
+                    , 1
+                    , "2018-06-11");
+            gr.save(gato);
+            System.out.println(gato);
             
-////            ***********************************UPDATE**************************
-//                  perro=pr.getByIdPerro(4);
-//                  perro.setNombre("frida");
-//                  perro.setRaza("pitbull");
-//                  perro.setEdad(5);
-//                  perro.setGenero(Genero.hembra);
-//                  perro.setIdVeterinario(5);
-//                  perro.setAdopcion(Adopcion.adoptado);
-//                  perro.setIdResponsable(8);//nota no permite el campo null
-//                  perro.setF_adopcion("2019-03-02");
-//                  pr.update(perro);
-//                  pr.getAll().forEach(System.out::println);//getAll
-//   
-//        } catch (Exception e) { e.printStackTrace();} 
+             //"**************************REMOVE*********************************");
+                       gr.remove(gr.getByIdGato(35));
+
+           
+             //"************************UPDATE***********************************");
+ 
+                       gato = gr.getByIdGato(2);
+                       gato.setNombre("lucas");
+                       gato.setRaza("mestizo");
+                       gato.setEdad(4);
+                       gato.setGenero(Genero.hembra);
+                       gato.setIdVeterinario(2);
+                       gato.setAdopcion(Adopcion.adoptado);
+                       gato.setIdResponsable(3);
+                       gato.setF_adopcion("2015-07-04");
+                       gr.update(gato);
+                       gr.getAll().forEach(System.out::println); // getALl
+   
+           } catch (Exception e) { e.printStackTrace(); }
     
-//        
-//        
-//        
+        
+        //**************************PERROS**************************************
+        try (Connection conn= Connector.getConnection()){
+            I_PerroRepository pr=new PerroRepository(conn);
+            
+        //********************SAVE****************************
+             Perro perro = new Perro(
+                  "kusturika"
+                 , "labrador"  
+                 , 11
+                 , Genero.hembra
+                 , 4
+                 , Adopcion.adoptado
+                 , 3
+                 ,"2018-10-23");
+            pr.save(perro);
+            System.out.println(perro);
+            
+//            **********************************REMOVE***************************
+            pr.remove(pr.getByIdPerro(3));
+            
+//            ***********************************UPDATE**************************
+                  perro=pr.getByIdPerro(4);
+                  perro.setNombre("frida");
+                  perro.setRaza("pitbull");
+                  perro.setEdad(5);
+                  perro.setGenero(Genero.hembra);
+                  perro.setIdVeterinario(5);
+                  perro.setAdopcion(Adopcion.adoptado);
+                  perro.setIdResponsable(8);//nota no permite el campo null
+                  perro.setF_adopcion("2019-03-02");
+                  pr.update(perro);
+                  pr.getAll().forEach(System.out::println);//getAll
+   
+        } catch (Exception e) { e.printStackTrace();} 
+    
+        
+        
+        
      //**************************REPONSABLES********************************   
     
         try (Connection conn= Connector.getConnection()){
@@ -129,39 +129,39 @@ public class TestRespositoryTp2 {
         } catch (Exception e) { e.printStackTrace(); }
 
         
-    }
-}
 
-//        //*********************************VETERINARIOS*******************    
-//        try (Connection conn= Connector.getConnection();){
-//            I_VeterinarioRepository vr= new VeterinarioRepository(conn);
-//            
-//            //*****************************SAVE*************************
-//            Veterinario veterinario = new Veterinario(
-//                    //nombre,apellido,email,turno,salario
-//                    "bruno"
-//                    ,"diaz"
-//                    ,"brudi@gmail.com"
-//                    ,Turno.tarde
-//                );
-//            vr.save(veterinario);
-//            System.out.println(veterinario);
-//            
-//            //*********************************REMOVE***************************
-//            vr.remove(vr.getByIdVeterinario(2));
-//            
-//            
-//            //**************************UPDATE*********************************
-//            veterinario=vr.getByIdVeterinario(4);
-//            veterinario.setNombre("clark");
-//            veterinario.setApellido("ken");
-//            veterinario.setEmail("clarken@gmail.com");
-//            veterinario.setTurno(Turno.tarde);
-//            //**********************getAll*************************************
-//            vr.getAll().forEach(System.out::println);
-//        } catch (Exception e) { e.printStackTrace();}
-//        
-//       
+//        *********************************VETERINARIOS*******************    
+        try (Connection conn= Connector.getConnection();){
+            I_VeterinarioRepository vr= new VeterinarioRepository(conn);
+            
+            //*****************************SAVE*************************
+            Veterinario veterinario = new Veterinario( 
+                    "bruno"
+                    , "diaz"
+                    , "brudi@gmail.com"
+                    , Turno.tarde
+            );
+                    //nombre,apellido,email,turno,salario
+     
+                
+            vr.save(veterinario);
+            System.out.println(veterinario);
+            
+            //*********************************REMOVE***************************
+            vr.remove(vr.getByIdVeterinario(2));
+            
+            
+            //**************************UPDATE*********************************
+            veterinario=vr.getByIdVeterinario(4);
+            veterinario.setNombre("clark");
+            veterinario.setApellido("ken");
+            veterinario.setEmail("clarken@gmail.com");
+            veterinario.setTurno(Turno.tarde);
+            //**********************getAll*************************************
+            vr.getAll().forEach(System.out::println);
+        } catch (Exception e) { e.printStackTrace();}
+
+       
     }
 
 }
